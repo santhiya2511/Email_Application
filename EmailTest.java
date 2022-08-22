0c6572809140075d6d8e14c3f0c1c8e4ed51b549
Santhiya M <111781402+santhiya2511@users.noreply.github.com>
2022-08-22 22:52:36 +0530

    Add files via upload

---
+++ EmailTest.java
@@ -0,0 +1,16 @@
+package EmailApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+	@Test
+	void EmailTest() {
+		EmailApplication junit = new EmailApplication();
+		
+	
+	}
+
+}
